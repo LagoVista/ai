1e27824c0a508517c581afdd0013f081ced01272
Kevin D. Wolf <kevinw@software-logistics.com>
2021-10-24 05:12:51 -0400

    comment out juypter notebook tests

--- tests/LagoVista.AI.Tests/Class1.cs
+++ tests/LagoVista.AI.Tests/Class1.cs
@@ -11,85 +11,87 @@ namespace LagoVista.AI.Tests
     [TestClass]
     public class JupyterNotebookAPITests
     {
-        EntityHeader _aiOrg;
-        EntityHeader _ai2Org;
-        EntityHeader _user;
+        // At some point we may want to revisit this, integration of Jupyter notebooks into NuvIoT
 
-        [TestInitialize]
-        public void Init()
-        {
-            _aiOrg = new EntityHeader()
-            {
-                Id = "ai"
-            };
+        //EntityHeader _aiOrg;
+        //EntityHeader _ai2Org;
+        //EntityHeader _user;
 
-            _ai2Org = new EntityHeader()
-            {
-                Id = "ai2"
-            };
+        //[TestInitialize]
+        //public void Init()
+        //{
+        //    _aiOrg = new EntityHeader()
+        //    {
+        //        Id = "ai"
+        //    };
 
-            _user = new EntityHeader()
-            {
+        //    _ai2Org = new EntityHeader()
+        //    {
+        //        Id = "ai2"
+        //    };
 
-            };
-        }
+        //    _user = new EntityHeader()
+        //    {
 
-        [TestMethod]
-        public async Task GetFiles()
-        {
-            var hubManager = new Mock<IHubManager>();
-            var hub = new Hub()
-            {
-                AccessToken = Environment.GetEnvironmentVariable("JPTY_ADMIN_TOKEN"),
-                Url = "ai-dev.iothost.net",
-                IsSecure = true
+        //    };
+        //}
 
-            };
+        //[TestMethod]
+        //public async Task GetFiles()
+        //{
+        //    var hubManager = new Mock<IHubManager>();
+        //    var hub = new Hub()
+        //    {
+        //        AccessToken = Environment.GetEnvironmentVariable("JPTY_ADMIN_TOKEN"),
+        //        Url = "ai-dev.iothost.net",
+        //        IsSecure = true
 
-            hubManager.Setup(call => call.GetHubForOrgAsync(It.IsAny<EntityHeader>(), It.IsAny<EntityHeader>())).ReturnsAsync(Core.Validation.InvokeResult<Hub>.Create(hub));
+        //    };
 
-            var connector = new NotebookConnector(hubManager.Object);
-            var files = await connector.GetFilesAsync(new Core.Models.EntityHeader()
-            {
-                Id = "ai2"
-            }, new Core.Models.EntityHeader()
-            {
+        //    hubManager.Setup(call => call.GetHubForOrgAsync(It.IsAny<EntityHeader>(), It.IsAny<EntityHeader>())).ReturnsAsync(Core.Validation.InvokeResult<Hub>.Create(hub));
 
-            });
+        //    var connector = new NotebookConnector(hubManager.Object);
+        //    var files = await connector.GetFilesAsync(new Core.Models.EntityHeader()
+        //    {
+        //        Id = "ai2"
+        //    }, new Core.Models.EntityHeader()
+        //    {
 
-            foreach (var file in files.Model)
-            {
-                Console.WriteLine(file.Name);
-            }
-        }
+        //    });
 
-        [TestMethod]
-        public async Task GetUserToken()
-        {
-            var hub = new Hub()
-            {
-                AccessToken = Environment.GetEnvironmentVariable("JPTY_ADMIN_TOKEN"),
-                Url = "ai-dev.iothost.net",
-                IsSecure = true
+        //    foreach (var file in files.Model)
+        //    {
+        //        Console.WriteLine(file.Name);
+        //    }
+        //}
+
+        //[TestMethod]
+        //public async Task GetUserToken()
+        //{
+        //    var hub = new Hub()
+        //    {
+        //        AccessToken = Environment.GetEnvironmentVariable("JPTY_ADMIN_TOKEN"),
+        //        Url = "ai-dev.iothost.net",
+        //        IsSecure = true
                
-            };
+        //    };
 
-            var hubManager = new Mock<IHubManager>();
-            hubManager.Setup(call => call.GetHubForOrgAsync(It.IsAny<EntityHeader>(), It.IsAny<EntityHeader>())).ReturnsAsync(Core.Validation.InvokeResult<Hub>.Create(hub));
+        //    var hubManager = new Mock<IHubManager>();
+        //    hubManager.Setup(call => call.GetHubForOrgAsync(It.IsAny<EntityHeader>(), It.IsAny<EntityHeader>())).ReturnsAsync(Core.Validation.InvokeResult<Hub>.Create(hub));
 
-            var hubConnector = new HubConnector(hubManager.Object);
-            var result = await hubConnector.GetOrgAccessToken(_aiOrg, _user);
-            Assert.IsTrue(result.Successful);
-            Console.WriteLine(result.Result.Token);
+        //    var hubConnector = new HubConnector(hubManager.Object);
+        //    var result = await hubConnector.GetOrgAccessToken(_aiOrg, _user);
+        //    Assert.IsTrue(result.Successful);
+        //    Console.WriteLine(result.Result.Token);
 
-            var notebookManager = new NotebookConnector(hubManager.Object);
+        //    var notebookManager = new NotebookConnector(hubManager.Object);
 
-            var files = await notebookManager.GetFilesAsync(_aiOrg, _user);
-            foreach(var file in files.Model)
-            {
-                Console.WriteLine(file);
-            }
+        //    var files = await notebookManager.GetFilesAsync(_aiOrg, _user);
+        //    foreach(var file in files.Model)
+        //    {
+        //        Console.WriteLine(file);
+        //    }
 
-        }
+        //}
     }
 }
