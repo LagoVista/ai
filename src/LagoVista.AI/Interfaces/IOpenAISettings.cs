cbfcae0e19c52a27212fd1096d5d6005f6e9f9cc
Kevin D. Wolf <kevinw@software-logistics.com>
2024-03-09 11:26:29 -0500

    Open AI Settings

---
+++ src/LagoVista.AI/Interfaces/IOpenAISettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LagoVista.AI.Interfaces
+{
+    public interface IOpenAISettings
+    {
+         public string OpenAIUrl { get;  }
+         public string OpenAIApiKey { get; }
+    }
+}
